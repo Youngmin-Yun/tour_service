b5e344e3250c02f1bbcb0784bad3e6b178c02c85
Youngmin-Yun <lauas15@naver.com>
2021-08-24 11:47:42 +0900

    여행 정보 서비스 작업 시작

---
+++ tourservice/src/main/java/com/tourService/TourserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tourService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourserviceApplication.class, args);
+	}
+
+}
